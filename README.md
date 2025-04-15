65abfd5dfef235cc5a44febd5716289f51320d35
Karicho21 <kkato1@charlotte.edu>
2025-04-14 23:54:24 -0400

    push2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 
+
 This program is for ITCS 4145 A5 Nbody Parallel for
+
+g++ npf.cpp -o gsc -I ~/rapidjson/include -lcurl
---
+++ npf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --job-name=4145-A1
+#SBATCH --partition=Centaurus
+#SBATCH --time=01:00:00
+#SBATCH --mem=16G
+
+
